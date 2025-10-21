460248f17f0a9c5cf87f806ca8d9a3f9af93fa82
Quentin <quentin.prunet@epita.fr>
2025-10-22 01:13:47 +0200

    feat: canary deployement for model predicting

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import mlflow
 import pandas as pd
+import random
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+CANARY_PROBABILITY = 0.1
+
 app = FastAPI()
 print("Loading model...")
 mlflow.set_tracking_uri("http://host.docker.internal:8080")
-model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/1")
+current_model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/1")
 print("Model loaded.")
 
 class InputData(BaseModel):
@@ -15,10 +18,18 @@ class InputData(BaseModel):
 
 @app.post("/predict")
 def predict(data: InputData):
-    return model.predict(pd.DataFrame(data.data)).tolist()
+    if random.random() < CANARY_PROBABILITY and "next_model" in globals():
+        return next_model.predict(pd.DataFrame(data.data)).tolist()
+    else:
+        return current_model.predict(pd.DataFrame(data.data)).tolist()
 
 @app.post("/update-model")
-def updafte_model():
-    global model
-    model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/latest")
+def update_model():
+    global current_model
+    global next_model
+    next_model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/latest")
     return {"status": "model updated"}
+
+@app.post("/accept-next-model")
+def accept_next_model():
+    current_model = next_model
